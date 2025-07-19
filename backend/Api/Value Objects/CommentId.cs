732822c183610f5818c0a6b60df5de58aa86fd7e
VlastaPeyton <vlastasokorac@gmail.com>
2025-07-19 18:44:29 +0200

    Strongly type id popravljen

--- backend/Api/Value Objects/CommentId.cs
+++ backend/Api/Value Objects/CommentId.cs
@@ -5,10 +5,11 @@
     {   
         public int Value { get; } // Nije Guid, jer app radi kada Id of Comment is int. Pa da ne moram brisati sve podatke i migrirati opet sve.
         private CommentId(int value) => Value = value; // Konstruktor na moderan nacin jer imam 1 polje samo
-
+        
+        // Of umesto public construktora je Ric domain model
         public static CommentId Of(int value) // Za sad je nepotreba jer u OnModelCreating automatski generisem Id polje Comments tabele
         {
-            ArgumentNullException.ThrowIfNull(value);
+            ArgumentNullException.ThrowIfNull(value); // Ovo je Domain layer i mora validacija u njemu
             return new CommentId(value);
         }
     }
--- backend/Api/Value Objects/StockId.cs
+++ backend/Api/Value Objects/StockId.cs
@@ -5,9 +5,10 @@
         public int Value { get; } // Nije Guid, jer app radi kada Id of Stock is int. Pa da ne moram brisati sve podatke i migrirati opet sve.
         private StockId(int value) => Value = value; // Konstruktor na moderan nacin jer imam 1 polje samo
 
-        public static StockId Of(int value)
+        // Of umesto public construktora je Ric domain model
+        public static StockId Of(int value) // Za sad mi ne treba ova metoda
         {
-            ArgumentNullException.ThrowIfNull(value);
+            ArgumentNullException.ThrowIfNull(value); // Ovo je Domain layer i mora validacija u njemu
             return new StockId(value);
         }
     }
