78f90cef71c9500f56d89f1b187d7e7b727ffbef
VlastaPeyton <vlastasokorac@gmail.com>
2025-10-12 12:51:19 +0200

    Sve radi

--- backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d18214cb74e34ef11eafccd5b4d9c7fdb03995e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24d01bab1234f602d39370fd9c48303147c8835")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
